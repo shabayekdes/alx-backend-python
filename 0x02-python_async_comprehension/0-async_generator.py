ae3380f6332855b3b46fba9d6a553907f44749e9
Esmail Shabayek <esmail.shabayek@gmail.com>
2024-04-24 00:15:04 +0200

    Add task 1

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Task async_generator
+"""
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """Loop 10 times, wait 1 sec each time"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
